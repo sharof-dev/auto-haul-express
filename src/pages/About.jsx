d3cf47b18a3a9a311ecaa957fff54315da40303f
sharof-dev <sharofbekmardiyev@gmail.com>
2024-07-04 15:39:25 +0500

    changed About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { Container, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import "../pages/about.scss";
 
 const About = () => {
   return (
-    <Container>
-      <Container className="container-first">
-        <Typography>About Us</Typography>
-        <Typography>
-          From preschool to pre-tertiary, our students enjoy fun, interactive
-          and relevant lessons and are empowered to think beyond the confines of
-          the classroom.
-        </Typography>
-      </Container>
-    </Container>
+    <Stack>
+      <Typography>About Us</Typography>
+      <Typography>
+        From preschool to pre-tertiary, our students enjoy fun, interactive
+        and relevant lessons and are empowered to think beyond the confines of
+        the classroom.
+      </Typography>
+    </Stack>
   );
 };
 
