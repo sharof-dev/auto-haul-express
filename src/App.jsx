4feda48c42530d618d5175b20092608894026d3b
Behruz <madaminovbehruz746@gmail.com>
2024-07-31 11:15:31 +0500

    added: car-shipping-cost page

--- src/App.jsx
+++ src/App.jsx
@@ -62,17 +62,6 @@ const App = () => {
         <Route path="/open-auto-transport" element={<OpenAuotTransport />} />
         <Route path="/snow-birds" element={<SnowBirds />} />
         <Route path="/student-auto" element={<StudentAuto />} />
-
-        {/* MY pages */}
-        <Route
-          path="/how-does-car-shipping-work/"
-          element={<CarShippingCost />}
-        />
-        <Route path="/car-shipping-costs/" element={<CrosCountry />} />
-        <Route
-          path="/cross-country-car-shipping/"
-          element={<HowDoesCarShipping />}
-        />
       </Routes>
       <Footer />
     </>
